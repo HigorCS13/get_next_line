809c35438c4e4c5d245e6a3253d89fadefafceb6
Higor Cunha Silva <hicunha-@c1r8s18.42porto.com>
2023-10-31 16:50:47 +0000

    get_nxt

---
+++ get_next_line.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hicunha- <hicunha-@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/31 11:38:48 by hicunha-          #+#    #+#             */
+/*   Updated: 2023/10/31 15:32:05 by hicunha-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_GET_NEXT_LINE_H
+# define FT_GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdarg.h>
+
+typedef struct s_list
+{
+	char			*str_buf;
+	struct s_list	*next;
+}	t_list;
+
+char	*get_next_line(int fd);
+
+#endif
