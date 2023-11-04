0952e58e7e314f70942fdd341152705651d63741
HigorCS13 <higor.c.silva13@gmail.com>
2023-11-04 14:20:45 +0000

    vai

--- get_next_line.c
+++ get_next_line.c
@@ -6,14 +6,63 @@
 /*   By: hicunha- <hicunha-@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/31 11:56:07 by hicunha-          #+#    #+#             */
-/*   Updated: 2023/11/04 10:14:06 by hicunha-         ###   ########.fr       */
+/*   Updated: 2023/11/04 11:29:40 by hicunha-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
+static void	append(t_list **list, char *buff)
+{
+	t_list	*new_node;
+	t_list	*last_node;
+
+	last_node = find_last_node(*list);
+	new_node = malloc(sizeof(t_list));
+	if (!new_node)
+		return ;
+	if (!last_node)
+		*list = new_node;
+	else
+		last_node->next = new_node;
+	new_node->str_buf = buff;
+	new_node->next = NULL;
+}
+
+static void	create_list(t_list **list, int fd)
+{
+	int		char_read;
+	char	*buff;
+
+	while (!found_newline(*list))
+	{
+		buff = malloc(BUFFER_SIZE + 1);
+		if (!buff)
+			return ;
+		char_read = read(fd, buff, BUFFER_SIZE);
+		if (!char_read)
+		{
+			free(buff);
+			return ;
+		}
+		buff[char_read] = '\0';
+		append(list, buff);
+	}
+}	
+
 char	*get_next_line(int fd)
 {
-	
+	static t_list	*list;
+	char			*next_line;
+
+	list = NULL;
+	if (fd <0 || BUFFER_SIZE <= 0 || read(fd, &next_line, 0) < 0)
+		return (NULL);
+	create_list(&list, fd);
+	if (list == NULL)
+		return (NULL);
+	next_line = get_line(list);
+	palish_list(&list);
+	return (next_line);
 }
 
