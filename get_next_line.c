fa86a7aa9a9c5737f7a35e16dad7e315182870da
Higor Cunha Silva <hicunha-@c1r8s18.42porto.com>
2023-11-06 21:32:58 +0000

    vamucarai

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: hicunha- <hicunha-@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/31 11:56:07 by hicunha-          #+#    #+#             */
-/*   Updated: 2023/11/06 13:50:49 by hicunha-         ###   ########.fr       */
+/*   Updated: 2023/11/06 20:35:08 by hicunha-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,10 +47,7 @@ static char	*get_line(t_list *list)
 	str_len = len_to_newline(list);
 	next_str = malloc(str_len + 1);
 	if (!next_str)
-	{
-		free(next_str);
 		return (NULL);
-	}
 	copy_str(list, next_str);
 	return (next_str);
 }
@@ -120,6 +117,9 @@ int	main()
 	lines = 1;
 	fd = open("lorem_ipsum.txt", O_RDONLY);
 	while ((line = get_next_line(fd)))
+	{
 		printf("%d->%s\n", lines++, line);
+		free(line);
+	}
 	return (0);
 }*/
